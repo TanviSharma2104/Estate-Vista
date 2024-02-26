a0025fe555bfe5f3ba359a5a1d53a69aa406db48
TanviSharma2104 <tanvisharma2703@gmail.com>
2024-02-26 23:04:39 +0530

    Profile Page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/about';
 import Header from './componenets/Header';
+import PrivateRoute from './componenets/PrivateRoute';
 
 
 export default function App() {
@@ -15,8 +16,10 @@ export default function App() {
     <Route path='/sign-in' element={<SignIn />} />
     <Route path='/sign-up' element={<SignUp />} />
     <Route path='/about' element={<About />} />
-    <Route path='/profile' element={<Profile />} />
     
+    <Route element={<PrivateRoute />} >
+      <Route path='/profile' element={<Profile />} /> 
+    </Route>
   </Routes>
   </BrowserRouter>
 }
