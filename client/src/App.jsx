46cf66efc033da65c98c616671b06cfcf48a8a48
TanviSharma2104 <tanvisharma2703@gmail.com>
2024-04-07 01:30:43 +0530

    fixed some error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
-import About from './pages/about';
+import About from './pages/About';
 import Header from './componenets/Header';
 import PrivateRoute from './componenets/PrivateRoute';
 import CreateListing from './pages/CreateListing';
