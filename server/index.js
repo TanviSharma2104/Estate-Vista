211716c0e12799d335d1c30dec860a481d6aa3da
TanviSharma2104 <tanvisharma2703@gmail.com>
2024-04-07 01:21:16 +0530

    changes for rendering

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+const __dirname=path.resolve();
 const app= express();
 app.use(express.json());
 
@@ -26,6 +29,10 @@ app.use("/server/user",userRouter);
 app.use("/server/auth",authRouter);
 app.use("/server/listing",listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+})
 
 app.use((err, req, res, next)=>{
     const statusCode=err.statusCode || 500;
